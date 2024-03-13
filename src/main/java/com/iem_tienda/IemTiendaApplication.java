96b08a51b62299f264e0b6af4a6bd4381239448e
cxldsakeb <jmarioulloav@gmail.com>
2024-03-12 20:43:55 -0600

    1 commit

---
+++ src/main/java/com/iem_tienda/IemTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.iem_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IemTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IemTiendaApplication.class, args);
+	}
+
+}
